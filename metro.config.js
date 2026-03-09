0a0365a74c423dbf78710dbd8a509ff4cc45295c
Krishnan K <krishnan.k@surveysparrow.com>
2026-03-09 16:09:03 +0530

    chore: Simplify Metro configuration and add development environment files
    - Updated metro.config.js to always enable Storybook integration
    - Modified package.json to streamline web start script
    - Added .codesandbox/tasks.json for task automation in CodeSandbox
    - Introduced .devcontainer/devcontainer.json for development container setup

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,9 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const { withStorybook } = require('@storybook/react-native/metro/withStorybook');
 const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-const storybookEnabled = process.env.STORYBOOK_ENABLED !== 'false';
-
-if (storybookEnabled) {
-  const {
-    withStorybook,
-  } = require('@storybook/react-native/metro/withStorybook');
-  module.exports = withStorybook(config, {
-    configPath: path.resolve(__dirname, '.rnstorybook'),
-  });
-} else {
-  module.exports = config;
-}
+module.exports = withStorybook(config, {
+  configPath: path.resolve(__dirname, '.rnstorybook'),
+});
